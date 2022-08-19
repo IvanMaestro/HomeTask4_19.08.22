39e3a20f56c91d743622059ed6c4e9d1098710bf
Masterov_IN <masterov_in@mail.ru>
2022-08-19 11:45:07 +0300

    Task25 done

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -3,3 +3,18 @@
 // и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите, в какую степень возвести число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int count = b;
+int result = a;
+
+for (int i = 1; i < b; i++)
+{
+    result = result * a;
+}
+int pow = result;
+Console.WriteLine($"{a} в {b} степени = {pow}");
