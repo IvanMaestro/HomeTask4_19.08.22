787fc68d389fc5a8c5ccc2d6e507a192ebb12b18
Masterov_IN <masterov_in@mail.ru>
2022-08-19 11:47:14 +0300

    Task27 done

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -5,3 +5,22 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DigitSum (int num)
+{
+int sum = 0;
+int tempDigit1 = 0;
+int tempDigit2 = num;
+
+for (; tempDigit2 > 0; )
+{
+    tempDigit1 = tempDigit2 % 10;
+    sum = sum + tempDigit1;
+    tempDigit2 = tempDigit2 / 10;
+}
+return sum;
+}
+int result = DigitSum(number);
+Console.WriteLine($"Сумма цифр в числе {number} = {result}");
